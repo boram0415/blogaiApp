d1894fb281b13f25cd11f54686b3d24f3c914ee2
boram0415 <boram04415@naver.com>
2026-03-26 22:32:14 +0900

    init: 기존 데이터 삭제 및 새 프로젝트 셋업 (보안파일 제외)

---
+++ src/test/kotlin/blogai/BlogaiApplicationTests.kt
@@ -0,0 +1,13 @@
+package blogai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogaiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
